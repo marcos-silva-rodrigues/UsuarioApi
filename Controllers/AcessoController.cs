1f78b5efdb910ac22edc2156b284afec4dac38e5
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-08-13 12:25:08 -0300

    feat: adicionando autenticação

---
+++ Controllers/AcessoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsuarioApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AcessoController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize(policy: "IdadeMinima")]
+        public IActionResult Get()
+        {
+            return Ok("Acesso permitido!");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using UsuarioApi.Authorization;
 using UsuarioApi.Data;
 using UsuarioApi.Services;
+using UsuariosApi.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,18 +18,46 @@ builder.Services.AddControllers();
 
 var connection = builder.Configuration.GetConnectionString("UsuarioConnection");
 builder.Services.AddDbContext<UsuarioDbContext>(opt => opt.UseMySql(connection, ServerVersion.AutoDetect(connection)));
+
 builder.Services
     .AddIdentity<Usuario, IdentityRole>()
     .AddEntityFrameworkStores<UsuarioDbContext>()
     .AddDefaultTokenProviders();
+
 builder.Services.AddAutoMapper
     (AppDomain.CurrentDomain.GetAssemblies());
+
 builder.Services.AddScoped<UsuarioService>();
 builder.Services.AddScoped<TokenService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddSingleton<IAuthorizationHandler, IdadeAuthorization>();
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(opts =>
+    {
+        opts.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("9ASHDA98H9ah9ha9H9A89n0f")),
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ClockSkew = TimeSpan.Zero
+            };
+    });
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("IdadeMinima", policy =>
+        policy.AddRequirements(new IdadeMinima(18))
+        );
+});
 
 var app = builder.Build();
 
@@ -35,6 +70,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
